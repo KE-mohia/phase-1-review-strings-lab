71aad57d417129fea438f3b0754bedb0c3263d62
wayne <keyawayne1@gmail.com>
2023-06-14 15:10:00 +0300

    completed assinment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+
+let currentUser="grass";
+
+ let welcomeMessage=`Welcome to Flatbook,  ${currentUser}!`;
+
+let excitedWelcomeMessage=welcomeMessage.toUpperCase();
+
+let shortGreeting="Welcome, "+currentUser[0]+"!";
+
+
+
+// const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = `Welome to flatbook,`;
+
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome ${currentUser[0]}`;
